cc2308a72999dbe31ec017064e1e1cb28e7ab9ea
Matthew Gong <matthew.zhigang.gong@gmail.com>
2020-11-22 14:36:06 +1300

    Practice Python multiprocessing

---
+++ python/multiprocessing-basics/process_example.py
@@ -0,0 +1,21 @@
+from multiprocessing import Process
+
+
+def print_func(continent="Asia"):
+    print("The name of continent is : ", continent)
+
+
+if __name__ == "__main__":
+    names = ["America", "Europe", "Africa"]
+    procs = []
+    proc = Process(target=print_func)
+    procs.append(proc)
+    proc.start()
+
+    for name in names:
+        proc = Process(target=print_func, args=(name,))
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
