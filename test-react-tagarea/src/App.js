3c4d25f0539446a202b3cbe2ccf0be5f3becd83c
Matthew Gong <matthew.zhigang.gong@gmail.com>
2020-12-24 12:18:21 +1300

    Add test-tag-area project

---
+++ test-react-tagarea/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Tagarea from "@mattzgg/react-tagarea";
+import Tagarea from "../node_modules/@mattzgg/react-tagarea/src/tagarea";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <Tagarea />
+        </div>
+    );
+}
+
+export default App;
