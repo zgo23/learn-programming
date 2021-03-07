a7b9dccdfdfd88899138c0a4a6edf2ab3cc3e788
Matthew Gong <matthew.zhigang.gong@gmail.com>
2021-03-07 16:32:19 +1300

    Make validation a module

---
+++ ts/playground-ts/src/namespaces/Validation.ts
@@ -0,0 +1,21 @@
+namespace Validation {
+    export interface StringValidator {
+        isAcceptable(s: string): boolean;
+    }
+
+    let lettersRegexp = /^[A-Za-z]+$/;
+    let numberRegexp = /^[0-9]+$/;
+
+    export class LettersOnlyValidator implements StringValidator {
+        isAcceptable(s: string) {
+            return lettersRegexp.test(s);
+        }
+    }
+    export class ZipCodeValidator implements StringValidator {
+        isAcceptable(s: string) {
+            return s.length === 5 && numberRegexp.test(s);
+        }
+    }
+}
+
+export default Validation;
--- ts/playground-ts/src/namespaces/Validators.ts
+++ ts/playground-ts/src/namespaces/Validators.ts
@@ -1,22 +1,4 @@
-namespace Validation {
-    export interface StringValidator {
-        isAcceptable(s: string): boolean;
-    }
-
-    let lettersRegexp = /^[A-Za-z]+$/;
-    let numberRegexp = /^[0-9]+$/;
-
-    export class LettersOnlyValidator implements StringValidator {
-        isAcceptable(s: string) {
-            return lettersRegexp.test(s);
-        }
-    }
-    export class ZipCodeValidator implements StringValidator {
-        isAcceptable(s: string) {
-            return s.length === 5 && numberRegexp.test(s);
-        }
-    }
-}
+import Validation from "./Validation";
 
 // Some samples to try
 let strings = ["Hello", "98052", "101"];
