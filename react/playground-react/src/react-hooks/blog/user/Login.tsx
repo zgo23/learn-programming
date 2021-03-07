3cb2ace8b4828bab2649c4470f9c5c8c1beb41ec
Matthew Gong <matthew.zhigang.gong@gmail.com>
2021-03-07 16:06:48 +1300

    Start to add state

--- react/playground-react/src/react-hooks/blog/user/Login.tsx
+++ react/playground-react/src/react-hooks/blog/user/Login.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import type { ChangeEvent } from "react";
+import type { SetUser } from "./UserBar";
 
-export default function Login() {
+export default function Login({ setUser }) {
+    const [username, setUsername] = useState("");
+
+    function handleUsername(evt: ChangeEvent<HTMLInputElement>) {
+        setUsername(evt.target.value);
+    }
+
     return (
-        <form onSubmit={(e) => e.preventDefault()}>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                setUser(username);
+            }}
+        >
             <label htmlFor="login-username">Username:</label>
-            <input type="text" name="login-username" id="login-username" />
+            <input
+                type="text"
+                name="login-username"
+                id="login-username"
+                value={username}
+                onChange={handleUsername}
+            />
             <label htmlFor="login-password">Password:</label>
             <input type="password" name="login-password" id="login-password" />
-            <input type="submit" value="Login" />
+            <input
+                type="submit"
+                value="Login"
+                disabled={username.length === 0}
+            />
         </form>
     );
 }
