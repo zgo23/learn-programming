48e431e75111f64df2ad513255dc38c03162c9ee
Matthew Gong <matthew.zhigang.gong@gmail.com>
2021-03-16 19:28:44 +1300

    Use multiple providers

--- react/playground-react/src/react-hooks/how-contexts-work/HowContextsWorkApp.tsx
+++ react/playground-react/src/react-hooks/how-contexts-work/HowContextsWorkApp.tsx
@@ -6,6 +6,9 @@ export default function HowContextsWorkApp() {
     return (
         <ThemeContext.Provider value={{ primaryColor: "coral" }}>
             <Header text="Hello World" />
+            <ThemeContext.Provider value={{ primaryColor: "deepskyblue" }}>
+                <Header text="This is a test" />
+            </ThemeContext.Provider>
         </ThemeContext.Provider>
     );
 }
